f3a6164db554656cffcbd3279e87b8a2dee6382a
Marcos Ruiz Muñoz <marcosrm99@gmail.com>
2021-03-30 13:59:03 +0200

    Social media buttons added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
+/*Slider Menu*/
+$(function() {
+    function slideMenu() {
+        var activeState = $("#menu-container .menu-list").hasClass("active");
+        $("#menu-container .menu-list").animate({ left: activeState ? "0%" : "-100%" }, 400);
+    }
+    $("#menu-wrapper").click(function(event) {
+        event.stopPropagation();
+        $("#hamburger-menu").toggleClass("open");
+        $("#menu-container .menu-list").toggleClass("active");
+        slideMenu();
+
+        $("body").toggleClass("overflow-hidden");
+    });
+
+    $(".menu-list").find(".accordion-toggle").click(function() {
+        $(this).next().toggleClass("open").slideToggle("fast");
+        $(this).toggleClass("active-tab").find(".menu-link").toggleClass("active");
+
+        $(".menu-list .accordion-content").not($(this).next()).slideUp("fast").removeClass("open");
+        $(".menu-list .accordion-toggle").not(jQuery(this)).removeClass("active-tab").find(".menu-link").removeClass("active");
+    });
+});
 var example = {
     data: [
         '<h1>Example 1</h1>',
@@ -57,26 +80,3 @@ window.onbeforeunload = function(e) {
 
     return 'Are you sure want to exit?';
 };
-
-$(function() {
-    function slideMenu() {
-        var activeState = $("#menu-container .menu-list").hasClass("active");
-        $("#menu-container .menu-list").animate({ left: activeState ? "0%" : "-100%" }, 400);
-    }
-    $("#menu-wrapper").click(function(event) {
-        event.stopPropagation();
-        $("#hamburger-menu").toggleClass("open");
-        $("#menu-container .menu-list").toggleClass("active");
-        slideMenu();
-
-        $("body").toggleClass("overflow-hidden");
-    });
-
-    $(".menu-list").find(".accordion-toggle").click(function() {
-        $(this).next().toggleClass("open").slideToggle("fast");
-        $(this).toggleClass("active-tab").find(".menu-link").toggleClass("active");
-
-        $(".menu-list .accordion-content").not($(this).next()).slideUp("fast").removeClass("open");
-        $(".menu-list .accordion-toggle").not(jQuery(this)).removeClass("active-tab").find(".menu-link").removeClass("active");
-    });
-}); // jQuery load
