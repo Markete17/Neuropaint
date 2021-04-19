798bc04f4f2c6ae9d242d1d50e2113304731d803
Marcos Ruiz Muñoz <marcosrm99@gmail.com>
2021-04-19 15:10:50 +0200

    New dimensions

--- js/index.js
+++ js/index.js
@@ -413,7 +413,7 @@ function expandPreview() {
         $(".title").css("-moz-transition", "all 0.75s ease-in-out");
         $(".title").css("-o-transition", "all 0.75s ease-in-out");
         $(".title").css("transition", "all 0.75s ease-in-out");
-        $(".title").css("margin-right", "40%");
+        $(".title").css("margin-left", "44.7%");
     }
     else {
         $(".editor").show("slow");
@@ -423,8 +423,8 @@ function expandPreview() {
         $("#btnexp").removeClass("fa fa-window-maximize");
         $("#btnexp").addClass("fa fa-expand");
         $(".zoombuttons").css("margin-left", "30.5%");
-
-        $(".title").css("margin-right", "27%");
+        $(".title").css("margin-left", "58.3%");
+        
     }
 }
 window.onbeforeunload = function (e) {
